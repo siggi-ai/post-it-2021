326bd581b20bfbfac4b85aedb1c22215692760d9
siggi-ai <82755519+siggi-ai@users.noreply.github.com>
2021-08-16 21:40:35 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = require("./router");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT ||3000;
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
